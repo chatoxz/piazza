aac8c7d2a918c27633399fded09512e6a37ee912
chatoxz <carrerasssebastian@gmail.com>
2017-03-05 22:54:13 -0300

    post venta algo

--- views/site/postventa.php
+++ views/site/postventa.php
@@ -6,5 +6,24 @@
  * Time: 18:35
  */
 ?>
+<div class=" post_venta_titulo">
+    <div class="container">POSTVENTA</div>
+</div>
 
-POSTVENTA
+
+
+<div class="container">Sabemos lo importante que es su auto para usted, es por ello que en nuestro servicio de post-venta ponemos a disposición de nuestros clientes lo mejor en equipamiento, repuestos originales y grupo humano a quienes les importa el servicio y la calidad del mismo. Es por ello que en nuestros talleres usted podrá asistir su unidad y mantenerla en perfectas condiciones.
+    Nuestro Taller de Post Venta cuenta con 1400 m2 de talleres y está ubicada en una zona accesible en la Ciudad de San Miguel de Tucumán.
+    Trabajamos con Mopar que es la marca de Servicio, Atención al cliente, Accesorios y Repuestos originales para todas las marcas del grupo FCA Automobiles.
+    Tu auto es una inversión importante. Si buscas un servicio de primera clase Fiat, lo vas a encontrar en PIAZZA. Nosotros creemos que el mantenimiento regular y chequeos de rutina frecuentes son la mejor forma de maximizar la eficiencia, seguridad, fiabilidad y rendimiento de tu auto.
+
+    Los técnicos de PIAZZA son certificados y capacitados en planta. Para determinar con precisión los problemas mecánicos y electrónicos se utilizan las últimas herramientas y tecnologías de diagnóstico. Nuestros mecánicos están preparados para manejar cualquier tipo de trabajo, desde una simple alineación, como una reparación de motor. También vas a tener la tranquilidad de saber que sólo trabajamos con piezas y accesorios originales.
+
+
+    SOLICITAR TURNO
+    Tel. 0381 423 3332 / 0381 452 7751 / 0381 452 7752
+    piazzarepuestos@piazzatucuman.com
+
+    Esto habría que agregar a la parte de post venta desde aquí también pueden solicitar turno.
+    http://www.fiatalertas.com.ar/TurnoPopUp.aspx?idConcesionario=65&oid=C-
+</div>
